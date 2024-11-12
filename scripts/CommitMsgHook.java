20c55f360e59c129d02734412c65db44cc6e9165
Sebastian Baltes <Sebastian.Baltes@uni-bayreuth.de>
2024-11-12 15:25:45 +0100

    Add demo code for next lecture

--- scripts/CommitMsgHook.java
+++ scripts/CommitMsgHook.java
@@ -1,13 +1,23 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class CommitMsgHook {
+    private static final Pattern linePattern = Pattern.compile("^([a-zA-Z0-9]+):.+$");
+
     public static void main(String[] args) {
         String commitMessage = args[0];
+        System.out.printf("Commit Message:\n%s\n\n", commitMessage);
 
-        if (commitMessage.isEmpty()) {
-            System.out.println("Commit message is invalid.");
-            System.exit(1);
+        Matcher lineMatcher = linePattern.matcher(commitMessage);
+
+        if (lineMatcher.matches()) { // match entire string
+            String title = lineMatcher.group(1);
+            System.out.printf("Title:\n%s\n\n", title);
+            System.out.println("Commit message is valid.");
+            System.exit(0);
         }
 
-        System.out.println("Commit message is valid.");
-        System.exit(0);
+        System.out.println("Commit message is invalid.");
+        System.exit(1);
     }
 }
