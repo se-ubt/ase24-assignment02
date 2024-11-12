09dfc7f4ae8410bfed2eb7ffb1a81ba62f78fc0f
Sebastian Baltes <Sebastian.Baltes@uni-bayreuth.de>
2024-11-12 16:30:14 +0100

    Add code to showcase Matcher.find()

--- scripts/CommitMsgHook.java
+++ scripts/CommitMsgHook.java
@@ -2,7 +2,7 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class CommitMsgHook {
-    private static final Pattern linePattern = Pattern.compile("^([a-zA-Z0-9]+):.+$");
+    private static final Pattern linePattern = Pattern.compile("^([a-zA-Z0-9]+):.+$"); // Pattern.MULTILINE
 
     public static void main(String[] args) {
         String commitMessage = args[0];
@@ -16,6 +16,18 @@ public class CommitMsgHook {
             System.out.println("Commit message is valid.");
             System.exit(0);
         }
+
+//        boolean matchFound = false;
+//        while (lineMatcher.find()) {// find next matching substring
+//            String title = lineMatcher.group(1);
+//            System.out.printf("Title:\n%s\n\n", title);
+//            matchFound = true;
+//        }
+//
+//        if (matchFound) {
+//            System.out.println("Commit message is valid.");
+//            System.exit(0);
+//        }
 
         System.out.println("Commit message is invalid.");
         System.exit(1);
